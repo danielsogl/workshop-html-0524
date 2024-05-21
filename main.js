12bcc31c554f3e9a126cb721f8217b80decc9d33
Daniel Sogl <me@danielsogl.de>
2024-05-21 14:33:19 +0200

    exercise #6

--- main.js
+++ main.js
@@ -30,8 +30,37 @@ function search() {
   const from = document.querySelector("#from").value;
   const to = document.querySelector("#to").value;
 
+  getFlightsFromApi(from, to).then((flights) => {
+    flights.forEach((flight) => {
+      addFlightToTable(flight);
+    });
+  });
+
   // showAlert(`Search from ${from} to ${to}`);
-  addFlightToTable({ from, to, id: "123", date: new Date() });
+  // addFlightToTable({ from, to, id: "123", date: new Date() });
+}
+
+/**
+ * @function getFlightsFromApi
+ * @param {string} from - The departure location
+ * @param {string} to - The destination location
+ * @returns {Promise<Flight[]>}
+ */
+function getFlightsFromApi(from, to) {
+  // build url with query params
+  const url = new URL("https://demo.angulararchitects.io/api/flight");
+  url.searchParams.append("from", from);
+  url.searchParams.append("to", to);
+
+  return (
+    fetch(url.href)
+      // returns another promise
+      .then((response) => response.json())
+      .catch((error) => {
+        // handle response errors and return an empty array of flights
+        return [];
+      })
+  );
 }
 
 /**
@@ -44,15 +73,15 @@ function addFlightToTable(flight) {
   const tableBody = table.getElementsByTagName("tbody")[0];
 
   // create table row with cols
-  const newRow = tableBody.insertRow(2);
+  const newRow = tableBody.insertRow();
   const idCell = newRow.insertCell(0);
   const fromCell = newRow.insertCell(1);
   const toCell = newRow.insertCell(2);
   const dateCell = newRow.insertCell(3);
 
   // set cell values
-  idCell.innerText = "1";
-  fromCell.innerText = "Bla";
-  toCell.innerText = "Bla";
-  dateCell.innerText = new Date().toLocaleDateString();
+  idCell.innerText = flight.id;
+  fromCell.innerText = flight.from;
+  toCell.innerText = flight.to;
+  dateCell.innerText = new Date(flight.date).toLocaleDateString();
 }
