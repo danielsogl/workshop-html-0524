2e5d062ec0ee746c2aba66f2f41f0a626378996e
Daniel Sogl <me@danielsogl.de>
2024-05-21 14:04:35 +0200

    exercise #5

--- main.js
+++ main.js
@@ -30,5 +30,29 @@ function search() {
   const from = document.querySelector("#from").value;
   const to = document.querySelector("#to").value;
 
-  showAlert(`Search from ${from} to ${to}`);
+  // showAlert(`Search from ${from} to ${to}`);
+  addFlightToTable({ from, to, id: "123", date: new Date() });
+}
+
+/**
+ * @function addFlight
+ * @returns {void}
+ */
+function addFlightToTable(flight) {
+  // select table body
+  const table = document.getElementById("flight-table");
+  const tableBody = table.getElementsByTagName("tbody")[0];
+
+  // create table row with cols
+  const newRow = tableBody.insertRow(2);
+  const idCell = newRow.insertCell(0);
+  const fromCell = newRow.insertCell(1);
+  const toCell = newRow.insertCell(2);
+  const dateCell = newRow.insertCell(3);
+
+  // set cell values
+  idCell.innerText = "1";
+  fromCell.innerText = "Bla";
+  toCell.innerText = "Bla";
+  dateCell.innerText = new Date().toLocaleDateString();
 }
